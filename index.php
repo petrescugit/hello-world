f8e08bf2e112189ce69ff75b5ba548ef279b2c0e
Victor Petrescu <petrescuresearch@gmail.com>
2023-12-30 17:53:27 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
-<?php
+<?php echo "Hello World!"; ?><br/>
 
-echo "Hello World!";
+<?php echo "You need to properly configure your DNS."; ?><br/>
 
-echo "You need to properly configure your DNS.";
-
-echo "c557f380f2a0d08e0da03343c978cfe439ecaf31afd57c539f832e862b418a02";
-/* by petrescu */
+<?php echo "c557f380f2a0d08e0da03343c978cfe439ecaf31afd57c539f832e862b418a02"; ?>
+<?php /* subdomain takeover by petrescu */ ?>
